7c0f7e13f38f84a56dd00502a2febfee39519b62
mihailuoih <71248435+mihailuoih@users.noreply.github.com>
2023-12-20 11:45:26 +0300

    Update voice_generator.py

--- voice_generator.py
+++ voice_generator.py
@@ -2,15 +2,14 @@ from vosk_tts import Model, Synth
 import os.path
 
 PATH_TO_MODEL = "/vosk-model-tts-ru-0.4-multi"
-PATH_TO_OUTPUT = "/"
 
 
 class VoiceGenerator:
     def __init__(self):
         self.model = Model(model_path=PATH_TO_MODEL)
 
-    def generate(self, text, file_name):
+    def generate(self, text, file_path, file_name):
         synth = Synth(self.model)
-        path = os.path.join(PATH_TO_OUTPUT, file_name)
+        path = os.path.join(file_path, file_name)
         synth.synth(text, path)
         return path
